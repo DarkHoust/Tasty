98c284da157d0d9d57386edabbbed19bd099a374
DarkHost <sultanisk@mail.ru>
2024-07-12 13:09:51 +0500

    Initial Commit

---
+++ Tasty/TastyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyApp.swift
+//  Tasty
+//
+//  Created by Sultan on 12.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TastyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
